11dde41682b2d3d84fda89ba15234cc55f6a3769
Abhi <Abhinavg916@gmail.com>
2020-12-18 12:59:35 +0530

    Web Scraping Notes

--- main.py
+++ main.py
@@ -1,80 +1,25 @@
-# Web Scraping using Python and BeautifulSoup
-
-## Introduction
-
-- Stages:
-
-  - Setting up Environment
-  - Get the HTML
-  - Parse the HTML
-  - HTML Tree traversal
-  - Use BeautifulSoup library to scrap data
-  - Save the result in `.csv` file
-
-- Ways to scrap a website:
-  - Use the API
-  - Scrap the HTML using tools (like Selenium) and libraries (like BeautifulSoup)
-
-#### Setting up Environment
-
-- Basic Requirements:
-
-  - Python with Environment Setup
-  - VSCode with Python (Microsoft) and Jupyter extensions
-  - Jupyter Notebook (Optional) (On VSCode/Browser)
-
-- `requests` library is used to make GET and POST request on the webpage
-- `html5lib` library is used to parse the HTML
-- `BeautifulSoup` library is used to manage the scrap of HTML
-
-```
-pip install requests
-pip install html5lib
-pip install bs4
-```
-
-#### Import Libraries and the set the destination URL to scrap
-
-```python
+# Step 0: Import Libraries and the set the destination URL to scrap
 import requests
 from bs4 import BeautifulSoup
 url="https://www.codewithharry.com"
-```
 
-## Get the HTML
-
-```python
+# Step 1: Get the HTML
 r = requests.get(url)
 htmlContent = r.content
 print(htmlContent)
-```
 
-## Parse the HTML
-
-```python
+# Step 2: Parse the HTML
 soup = BeautifulSoup(htmlContent, 'html.parser')
 print(soup)
 print(soup.prettify) # .prettify formats the code well with proper indents
-```
 
-## HTML Tree Traversal (DOM) & Use of BeautifulSoup to scrap the HTML
-
-#### Get the title of HTML page
-
-```python
+# Step 3: HTML Tree traversal (DOM)
+# Get the title of HTML page
 title = soup.title
 print(title) # Prints the <title> tag on console
 print(title.string) # Prints the string of the <title> on console
-```
 
-#### Commonly used types of objects in BeautifulSoup
-
-- Tag
-- NavigableString - It is different from normal string in Python because it comes with in-build special functions
-- BeautifulSoup
-- Comment
-
-```python
+# Commonly used types of objects in BeautifulSoup library
 print(type(title)) # Prints the type on console. Here, it is Tag <class 'bs4.element.Tag'>
 print(type(title.string)) # Prints the type on console. Here, it is String <class 'bs4.element.NavigableString'>
 print(type(soup)) # Here, it is BeautifulSoup object <class 'bs4.BeautifulSoup'>
@@ -84,135 +29,80 @@ print(soup2.p) # Prints the p tag on console
 print(soup2.p.string) # Prints the content of p tag on console ("this is a comment")
 print(type(soup2.p.string)) # Here, it is Comment <class 'bs4.element.Comment'>
 # exit() # To terminate the execution of program at any time
-```
 
-#### Get all the paragrahps from the page
-
-```python
+# Get all the paragrahps from the page
 paras = soup.find_all('p')
 print(paras)
-```
 
-#### Get all the anchors from the page
-
-```python
+# Get all the anchors from the page
 anchors = soup.find_all('a')
 print(anchors)
-```
 
-#### Get only the first paragraph from the page
-
-```python
+# Get only the first paragraph from the page
 para = soup.find('p')
 print(para)
-```
 
-#### Get the class of any tag from the page
-
-```python
+# Get the class of any tag from the page
 paraClass = (soup.find('p')['class'])
 print(paraClass)
-```
 
-#### Find all the elements with class "specific-name"
-
-```python
+# Find all the elements with class "specific-name"
 print(soup.find_all("p", class_="lead"))
-```
 
-#### Get the text from the tags
-
-```python
+# Get the text from the tags
 print(soup.find('p').get_text())
-```
 
-#### Get the text from the soup (entire HTML)
-
-```python
+# Get the text from the soup (entire HTML)
 print(soup.get_text())
-```
 
-#### Get all the links in anchors from the page
-
-```python
+# Get all the links in anchors from the page
 for link in anchors:
     print(link.get('href'))
-```
 
-#### To avoid the duplication and have hyperlinks in console
-
-```python
+# To avoid the duplication and have hyperlinks in console
 all_links = set()
 for link in anchors:
     if(link.get('href')!='#'):
         linkText = "https://codewithharry.com" + link.get('href')
         all_links.add(link)
         print(linkText)
-```
 
-#### To get the element by ID
-
-```python
+# To get the element by ID
 navbarSupportedContent = soup.find(id="navbarSupportedContent")
 print(navbarSupportedContent)
-```
 
-#### To get the children and contents of the div
-
-```python
+# To get the children and contents of the div
 print(navbarSupportedContent.children)
 print(navbarSupportedContent.contents) # Prints all content elements/tag inside the div in form of list
 for elem in navbarSupportedContent.contents: # Prints all content one by one using loops
     print(elem)
 for elem in navbarSupportedContent.children: # Prints the same as contents
     print(elem)
-```
+# The difference between children and content
+# `.contents` - A tag's children are available as a list
+# `.children` - A tag's children are available as a generator
+# List uses memory to store.
+# For very big large webpages, `.children` is more efficient by taking less space in memory
 
-- The difference between children and content
-  `.contents` - A tag's children are available as a list
-  `.children` - A tag's children are available as a generator
-- List uses memory to store.
-- For very big large webpages, `.children` is more efficient by taking less space in memory
-
-```python
 for item in navbarSupportedContent.strings:
     print(item)
 
 for item in navbarSupportedContent.stripped_strings: # Strips the string for ease of use
     print(item)
-```
 
-#### To get the parent
-
-```python
+# To get the parent
 print(navbarSupportedContent.parent) # Immediate Parents only
 print(navbarSupportedContent.parents) # Generator object is displayed that menas it can be iterated
 for item in navbarSupportedContent.parents:
     # print(item)
     print(item.name)
-```
 
-#### To get the siblings
-
-```python
+# To get the sibling
 print(navbarSupportedContent.next_sibling.next_sibling)
 print(navbarSupportedContent.previous_sibling.previous_sibling)
-```
 
-#### Use of CSS Selectors to target the elementss
-
-```python
+# Use of CSS Selectors to target the elementss
 elem = soup.select('#loginModal')
 print(elem)
 elem = soup.select('.modal-footer')
 print(elem)
-```
-
-## Save the result in .csv file
-
----
-
-# Author
-
-- Name - Abhinav
-- GitHub - [github.com/abhinavg916](https://github.com/abhinavg916)
